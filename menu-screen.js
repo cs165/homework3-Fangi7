a79ae230a67a14fe94a4f5eec9c5f32af4037499
Fangi <icedrip7@gmail.com>
2019-05-10 03:01:58 +0800

    Update menu-screen.js

--- menu-screen.js
+++ menu-screen.js
@@ -7,15 +7,50 @@
 // - Adding additional fields
 
 class MenuScreen {
-  constructor(containerElement) {
+  constructor(containerElement,mainElement, resultElement) {
+    this.mainElement = mainElement
     this.containerElement = containerElement;
-  }
+    this.resultElement = resultElement;
+    this.show = this.show.bind(this);
+    this.hide = this.hide.bind(this);
+    this.createDiv = this.createDiv.bind(this);
+    this.menuClick = this.menuClick.bind(this);
 
-  show() {
+  }
+  createDiv(menuChoice){
+    const newdiv = document.createElement('div');
+    //use context?
+    newdiv.innerHTML = menuChoice.title;
+    return newdiv;
+  }
+   menuClick(){
+    console.log(this);
+    this.flashcards = new FlashcardScreen(this.mainElement,this.resultElement,this.show);
+    this.flashcards.createShow();
+    this.hide();
+  }
+  show(){
     this.containerElement.classList.remove('inactive');
+  }
+  createShow() {
+    var choicesVar = document.querySelector('#choices');
+    console.log(choicesVar);
+    //create new child
+    console.log(FLASHCARD_DECKS.length);
+    for(let i = 0; i < FLASHCARD_DECKS.length;i++){
+      const newMenu = FLASHCARD_DECKS[i];
+      const newDiv = this.createDiv(newMenu);
+      console.log(newMenu);
+      console.log(newDiv);
+
+      newDiv.addEventListener('click',this.menuClick);
+      choicesVar.appendChild(newDiv);
+    }
+
   }
 
   hide() {
     this.containerElement.classList.add('inactive');
+    //newDiv.removeEventListener('click',this.menuClick);
   }
 }
